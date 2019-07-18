6b82e7232d255a60171bdae11ddbd4da595499fc
unknown <awatson23>
2019-07-18 12:39:57 -0400

    first folder push

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Cadence's Portfolio</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+  
+    </body>
+</html>
