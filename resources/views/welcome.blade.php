a9151c73565cf346ceefe1aaff53728d1c7f2593
unknown <awatson23>
2019-07-29 15:25:01 -0400

    begging basic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,53 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="reset.css">
-        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Darker+Grotesque&display=swap" rel="stylesheet"> 
     </head>
     <body>
-  
+        <header id="header">
+            <div id="logoSpace"><img src="images/watsonLogo.svg"></div>
+                <nav id="mainNav">
+                    <ul id="navList">
+                        <li><a href="linkedIn.com">linkedIn</a></li>
+                        <li class="navSeparator"> / </li>
+                        <li><a href="/">hello@cadencewatson.com</a></li>
+                    </ul>
+                </nav>
+        </header>
+
+        <section class="hero">
+            <div id="heroIMG">
+                
+                <p id="heroText">data visualization</p>
+            </div>
+        </section>
+
+        <section id="projects">
+            <div id="projectsTitle">
+                <h2 class="subTitle">Featured works</h2>
+                <p class="copy">design and development</p>
+            </div>
+            <div id="projGridDiv">
+                <div class="projGrid">1</div>
+                <div class="projGrid">2</div>
+                <div class="projGrid">3</div>
+                <div class="projGrid">4</div>
+                <div class="projGrid">5</div>
+                <div class="projGrid">6</div>
+            </div>
+        </section>
+
+        <section id="skills">
+            <div id="skillsTitle">
+                <h2 class="subTitle">Skills</h2>
+                <p class="copy">Services I have to offer</p>
+            </div>
+            <div id="skillsDisplay">
+                <img id="skillsChart" src="images/skills.svg">
+                <div id="chartCover"></div>
+            </div>
+        </section>
     </body>
 </html>
